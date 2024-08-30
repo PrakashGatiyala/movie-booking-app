8db6fba44cf24056c9296611139185b1c88f34da
Prakash <gatiyalap@gmail.com>
2024-08-31 00:34:23 +0530

    Add Payment Verify API

--- services/booking.service.js
+++ services/booking.service.js
@@ -1,10 +1,12 @@
 const {
   createBookingValidator,
+  verifyBookingPaymentValidator,
 } = require('../lib/validators/booking.validator')
 const Booking = require('../models/booking.model')
 const AppError = require('../errors/app.error')
 const TheatreHallMovieMapping = require('../models/theatre-hall-movie-mapping.model')
 const TheatreHall = require('../models/theatre-halls.model')
+const { hash } = require('../utils/hash')
 const Razorpay = require('razorpay')
 
 const razorpay = new Razorpay({
@@ -61,6 +63,11 @@ class BookingService {
         amount,
         currency: 'INR',
         receipt: `booking-${showId}-${seatNumber}`,
+        notes: {
+          userId,
+          showId,
+          seatNumber,
+        },
       }
       const order = await razorpay.orders.create(options)
       return order
@@ -68,7 +75,53 @@ class BookingService {
       if (error instanceof AppError) {
         throw error
       }
-      console.log(`Error creating booking`, error)
+      console.log(`Error creating order for Booking`, error)
+      throw new AppError('Internal server error', 500)
+    }
+  }
+
+  static async verifyBookingPayment(paymentData) {
+    const safeParsedData = await verifyBookingPaymentValidator.safeParseAsync(
+      paymentData
+    )
+    if (!safeParsedData.success) {
+      throw new AppError(safeParsedData.error, 400)
+    }
+    const { paymentId, orderId, signature } = safeParsedData.data
+    const RAZORPAY_API_SECRET = process.env.RAZORPAY_API_SECRET
+    const expectedSignature = hash(
+      `${orderId}|${paymentId}`,
+      RAZORPAY_API_SECRET,
+      'sha256'
+    )
+    if (signature !== expectedSignature) {
+      throw new AppError('Invalid signature', 400)
+    }
+    const paymentInfo = await razorpay.payments.fetch(paymentId)
+    const { status, notes } = paymentInfo
+    if (status !== 'captured') {
+      throw new AppError(`Payment not captured`, 400)
+    }
+    const { userId, showId, seatNumber } = notes
+    try {
+      const booking = await Booking.create({
+        userId,
+        showId,
+        seatNumber,
+        paymentId,
+        geteway: 'RAZORPAY',
+      })
+      return booking
+    } catch (error) {
+      // Refund the payment if booking creation fails
+      // await razorpay.payments.refund(paymentId, {
+      //   amount: paymentInfo.amount,
+      //   speed: 'normal'
+      // })
+      if (error instanceof AppError) {
+        throw error
+      }
+      console.log(`Error verifing Payment for booking`, error)
       throw new AppError('Internal server error', 500)
     }
   }
